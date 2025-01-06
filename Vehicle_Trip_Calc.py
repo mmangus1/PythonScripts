009485b5fca3e54227189bd98910344382571b74
Matthew Mangus <103114956+mmangus1@users.noreply.github.com>
2025-01-05 21:38:56 -0500

    Update Vehicle_Trip_Calc.py
    Added a class docstring

--- Vehicle_Trip_Calc.py
+++ Vehicle_Trip_Calc.py
@@ -10,6 +10,41 @@ import sys
 
 
 class Vehicle:
+    """
+    A class used to represent a Vehicle.
+
+    ...
+
+    Attributes
+    ----------
+    Auto_Style : str
+        Style of vehicle.
+    Make : str
+        Make of vehicle.
+    Model : str
+        Model of vehicle.
+    Color : int
+        Color of vehicle.
+    Amount_Tank : float
+        Amount of fuel the vehicles tank holds.
+    Epa_Mpg : float
+        Environmental protection agency miles per gallon estimation.
+    Actual_Mpg : float
+        Actual miles per gallon.
+    Number_Wheels : int
+        Number of wheels.
+    Number_Doors : int
+        Number of doors.
+    Total_Miles : float
+        Number of miles per a trip.
+    Total_Length : float
+        Total Length of trip in miles.
+
+
+    Methods
+    -------
+
+    """
     Auto_Style = str()
     Make = str()
     Model = str()
@@ -97,47 +132,47 @@ def main() -> None:
                     or new_vehicle == 'no'):
                 sys.exit()
             else:
-                Vehicle1 = Vehicle()
+                vehicle_1 = Vehicle()
                 print('Enter Automobile Style:')
-                Vehicle1.Auto_Style = str(input())
+                vehicle_1.Auto_Style = str(input())
                 print('Enter Automobile Make:')
-                Vehicle1.Make = str(input())
+                vehicle_1.Make = str(input())
                 print('Enter Automobile Model:')
-                Vehicle1.Model = str(input())
+                vehicle_1.Model = str(input())
                 print('Enter Automobile Color:')
-                Vehicle1.Color = str(input())
+                vehicle_1.Color = str(input())
                 print('Enter Automobile Gas Tank Capacity:')
-                Vehicle1.Amount_Tank = float(input())
+                vehicle_1.Amount_Tank = float(input())
                 print('Enter EPA MPG Estimation:')
-                Vehicle1.Epa_Mpg = float(input())
+                vehicle_1.Epa_Mpg = float(input())
                 print('Enter Actual MPG:')
-                Vehicle1.Actual_Mpg = float(input())
+                vehicle_1.Actual_Mpg = float(input())
                 print('Enter Number of Wheels:')
-                Vehicle1.Number_Wheels = int(input())
+                vehicle_1.Number_Wheels = int(input())
                 print('Enter Number of Doors:')
-                Vehicle1.Number_Doors = int(input())
+                vehicle_1.Number_Doors = int(input())
                 print('Enter Total Miles Traveled:')
-                Vehicle1.Total_Miles = float(input())
+                vehicle_1.Total_Miles = float(input())
                 print('Enter Total Length of Trip:')
-                Vehicle1.Total_Length = float(input())
+                vehicle_1.Total_Length = float(input())
 
                 # Perform Calculations for Output:
                 var_gallons = gallons_used(
-                    Vehicle1.Total_Miles, Vehicle1.Actual_Mpg)
+                    vehicle_1.Total_Miles, vehicle_1.Actual_Mpg)
                 var_miles = fuel_required(
-                    Vehicle1.Total_Miles, Vehicle1.Actual_Mpg)
+                    vehicle_1.Total_Miles, vehicle_1.Actual_Mpg)
                 var_tanks = tanks_consumed(
-                    Vehicle1.Total_Miles, Vehicle1.Amount_Tank)
+                    vehicle_1.Total_Miles, vehicle_1.Amount_Tank)
                 var_carbon = total_carbon(
-                    Vehicle1.Total_Miles, Vehicle1.Actual_Mpg)
+                    vehicle_1.Total_Miles, vehicle_1.Actual_Mpg)
 
-                print(f'{Vehicle1.Make} {Vehicle1.Model} '
+                print(f'{vehicle_1.Make} {vehicle_1.Model} '
                       f'has used {var_gallons} gallons of fuel')
-                print(f'{Vehicle1.Make} {Vehicle1.Model} '
+                print(f'{vehicle_1.Make} {vehicle_1.Model} '
                       f'has {var_miles} miles left before fuel is needed')
-                print(f'{Vehicle1.Make} {Vehicle1.Model} '
+                print(f'{vehicle_1.Make} {vehicle_1.Model} '
                       f'has consumed {var_tanks} tanks of fuel')
-                print(f'{Vehicle1.Make} {Vehicle1.Model} '
+                print(f'{vehicle_1.Make} {vehicle_1.Model} '
                       f'has emitted {var_carbon} of carbon')
 
     except SyntaxError as err:
